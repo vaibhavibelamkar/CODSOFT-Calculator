b861cb751a311ebe86363bb0948850004a210ba6
unknown <vaibhavi.belamkar22@gmail.com>
2023-11-30 00:03:51 +0000

    first commit

---
+++ cal.js
@@ -0,0 +1,17 @@
+function calculate(val) {
+    var v = document.getElementById('res');
+    v.value += val;
+ }
+ function result() {
+    var num1 = document.getElementById('res').value;
+    var num2 = eval(num1);
+    document.getElementById('res').value = num2;
+ }
+ function clear_data() {
+    var inp = document.getElementById('res');
+    inp.value = '';
+ }
+ function back() {
+    var ev = document.getElementById('res');
+    ev.value = ev.value.slice(0,-1);
+ }
